8a99722275634da0078d5dd898072caeec51bd2e
pdilipc <dilip.pc@gmail.com>
2022-11-17 13:05:40 +0400

    No major changes - so chill

--- rhyme.py
+++ rhyme.py
@@ -1,2 +1,2 @@
-for x in range(5,1,-1);
+for x in range(6,1,-1):
   print (x, 'little monkeys jumping on the bed, 1 fell off and bumped his head, momma called the doctor and the doctor said, no more monkeys jumping on the bed')
